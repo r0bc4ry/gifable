6f35cd0e424f7c71dbd49a6e3b399a3137557952
Rob Cary <rcary@heyo.com>
2015-03-05 01:53:06 -0500

    Dumping shell_exec to STDOUT

--- app/Commands/TranscodeGifCommand.php
+++ app/Commands/TranscodeGifCommand.php
@@ -40,8 +40,8 @@ class TranscodeGifCommand extends Command implements SelfHandling, ShouldBeQueue
         $outputFilePath = sys_get_temp_dir() . '/' . $this->gif->shortcode;
 
         // Transcode GIF to WebM and MP4
-        shell_exec('~/bin/ffmpeg -i "' . $this->gif->gif_http_url . '" -c:v libvpx -qmin 0 -qmax 50 -crf 5 -b:v ' . $targetBitrate . 'k -an ' . $outputFilePath . '.webm');
-        shell_exec('~/bin/ffmpeg -i "' . $this->gif->gif_http_url . '" -c:v libx264 -preset slow -crf 18 -an ' . $outputFilePath . '.mp4');
+        var_dump(shell_exec('~/bin/ffmpeg -i "' . $this->gif->gif_http_url . '" -c:v libvpx -qmin 0 -qmax 50 -crf 5 -b:v ' . $targetBitrate . 'k -an ' . $outputFilePath . '.webm 2>&1'));
+        var_dump(shell_exec('~/bin/ffmpeg -i "' . $this->gif->gif_http_url . '" -c:v libx264 -preset slow -crf 18 -an ' . $outputFilePath . '.mp4 2>&1'));
 
         // Upload WebM and MP4 files to Rackspace
         $rackspaceService = new RackspaceService();
