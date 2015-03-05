9495b66f591fa102730d9733cacd3d1b797cb7d0
Rob Cary <rcary@heyo.com>
2015-03-05 17:40:08 -0500

    Added meta tag fallback and MP4 file

--- resources/views/gif.blade.php
+++ resources/views/gif.blade.php
@@ -1,13 +1,17 @@
 @extends('layouts/http')
 
 @section('head')
+    <meta property="og:title" content="Gifable">
     <meta property="og:site_name" content="Gifable">
     <meta property="og:url" content="{{ action('IndexController@getGif', ['gif' => $gif->shortcode]) }}">
+    <meta property="og:description" content="Squash your GIFs into super efficient HTML5 videos that can be viewed from any device.">
     <meta property="og:image" content="{{ $gif->png_url }}">
 
     <meta property="og:type" content="video.other">
-    <meta property="og:video:url" content="{{ $gif->webm_url }}">
-    <meta property="og:video:type" content="video/webm">
+    <meta property="og:video:url" content="{{ $gif->mp4_url }}">
+    <meta property="og:video:type" content="video/mp4">
+    <meta property="og:video:url" content="{{ action('IndexController@getGif', ['gif' => $gif->shortcode]) }}">
+    <meta property="og:video:type" content="text/html">
     <meta property="og:video:width" content="{{ $gif->width }}">
     <meta property="og:video:height" content="{{ $gif->height }}">
 @stop
