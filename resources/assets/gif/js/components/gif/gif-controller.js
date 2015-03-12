7f47c051450f5b7ef2dce35da4ec93c03abe5dfc
Rob Cary <rcary@heyo.com>
2015-03-11 23:52:52 -0400

    Updated dialog scope and close handling

--- resources/assets/gif/js/components/gif/gif-controller.js
+++ resources/assets/gif/js/components/gif/gif-controller.js
@@ -12,13 +12,21 @@ angular.module('gifable.gif.controllers')
 
             ngDialog
         ) {
+            $scope.tagDialog = {};
             $scope.tags = $window.$tags;
 
-            $scope.showTagDialog = function(message) {
+            $scope.showTagDialog = function() {
+                $scope.tagDialog.newTag = '';
+
                 ngDialog.open({
                     template: 'components/gif/add-tag-dialog.html',
                     scope: $scope
                 });
+            };
+
+            $scope.tagDialog.addTag = function() {
+                console.log($scope.tagDialog.newTag);
+                ngDialog.closeAll();
             };
         }
     ]);
